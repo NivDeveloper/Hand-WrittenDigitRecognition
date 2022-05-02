dacbe07f32bf0e86e912cb94471e938877af880e
Nivan <pknniv001@uct.ac.za>
2022-05-02 22:47:54 +0200

    added function to test jpg images

--- firstClassifier.py
+++ firstClassifier.py
@@ -11,7 +11,13 @@ import torchvision
 def main():
     # Get cpu or gpu device for training.
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    batch_size = 128
+    epochs = 10
+    model = NeuralNetwork().to(device)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
     
+    print(model)
     #SETTING UP DATA##################################
     # Download training data from open datasets.
     training_data = datasets.MNIST(
@@ -35,56 +41,17 @@ def main():
     ###################################################
     
     #training network#############################################
-    batch_size = 128
-    model = NeuralNetwork().to(device)
-    print(model)
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
-    epochs = 10
+
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
-        train(train_dataloader, model, loss_fn, optimizer)
+        train(train_dataloader, model, loss_fn, optimizer, device)
     ##############################################################
-    test(test_dataloader, model)
+    test(test_dataloader, model, loss_fn, device)
     print("Done!")
 
-    image = test_jpg("./MNIST_JPGS/testSample/img_1.jpg")
+    imagepred = test_jpg("./MNIST_JPGS/testSample/img_1.jpg", model, device)
+    print(imagepred)
         
-
-def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
-
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-def test(dataloader, model):
-    size = len(dataloader.dataset)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            #print(pred,y)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= size
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-    
 # Define model
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -104,15 +71,54 @@ class NeuralNetwork(nn.Module):
         logits = self.network(x)
         return logits
 
-def test_jpg(image_name):
+
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            #print(pred,y)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= size
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    
+def test_jpg(image_name, model, device):
+    
     image = PIL.Image.open(image_name)
     loader = Compose([torchvision.transforms.PILToTensor()])
     image = loader(image).float()
     image = image.unsqueeze(0)
-    if torch.cuda.is_available():
-        return image.cuda()
-    else:
-        return image
+    image = image.to(device)
+    with torch.no_grad():
+        pred = model(image)
+    
+    return pred.argmax().item()
+        
+        
     
 
 
